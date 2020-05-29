84a6363fcab267c6017255ac41b5ca4ede63e948
qiao <3530078622@qq.com>
2020-05-29 10:11:26 +0800

    更新

--- myhouduan/api/ceshi.js
+++ myhouduan/api/ceshi.js
@@ -2,7 +2,8 @@ const msg = require('../model/msg.js')
 async function ceshi(req,res){
 	var a = await msg.create({username:'123'})
 	if(a){
-		res.send('添加成功')
+		var b = await msg.find()
+		res.send(b)
 	}else{
 		res.send('添加失败')
 	}
--- myhouduan/app.js
+++ myhouduan/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 const db = require('./model/db')
+const cors = require('cors') 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
